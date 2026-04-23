951b8b6fdae86da9f60b650207615db898578bed
Financier <mir.mir.01.2023@gmail.com>
2026-04-23 11:11:03 +0300

    feat: add collection gradients norm

--- src/train.py
+++ src/train.py
@@ -14,20 +14,34 @@ def train_one_epoch(
     criterion: nn.Module,
     optimizer: optim.Optimizer,
     device: torch.device,
-) -> float:
+) -> tuple[float, float]:
     model.train()
     tot_loss = 0.0
+    tot_grad_norm = 0.0
 
     for x, y in loader:
         x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
         loss = criterion(model(x), y)
         loss.backward()
+
+        # ========== тут собираем градиенты ==========
+        batch_norm = 0.0
+        for p in model.parameters():
+            if p.grad is not None:
+                param_norm = p.grad.detach().data.norm(2)
+                batch_norm += param_norm.item() ** 2
+        batch_norm = batch_norm**0.5
+        tot_grad_norm += batch_norm
+        # ========== конец ==========
+
         # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)  # let it be
         optimizer.step()
         tot_loss += loss.item()
 
-    return tot_loss / len(loader)
+    avg_loss = tot_loss / len(loader)
+    avg_grad = tot_grad_norm / len(loader)
+    return avg_loss, avg_grad
 
 
 @torch.no_grad()
@@ -58,18 +72,27 @@ def train_model(
 
     model.to(device)
 
-    history = {"train": [], "val": []}
+    history = {"train": [], "val": [], "grad_norm": []}
 
     pbar = tqdm(range(epochs), desc="Training", leave=True)
     for epoch in pbar:
-        train_loss = train_one_epoch(model, train_loader, criterion, optimizer, device)
+        train_loss, grad_norm = train_one_epoch(
+            model, train_loader, criterion, optimizer, device
+        )
         val_loss = evaluate(model, val_loader, criterion, device)
 
         # scheduler.step()
 
         history["train"].append(train_loss)
         history["val"].append(val_loss)
+        history["grad_norm"].append(grad_norm)
 
-        pbar.set_postfix({"train": f"{train_loss:.4f}", "val": f"{val_loss:.4f}"})
+        pbar.set_postfix(
+            {
+                "train": f"{train_loss:.4f}",
+                "val": f"{val_loss:.4f}",
+                "grad": f"{grad_norm:.4f}",
+            }
+        )
 
     return history
